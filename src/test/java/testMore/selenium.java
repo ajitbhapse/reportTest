cf8bb9874cdf742858a6f15bfe7fecc8fbb3b6f7
ajit <ajitbhapse@gmail.com>
2017-03-22 01:47:27 +0530

    added more test

---
+++ src/test/java/testMore/selenium.java
@@ -0,0 +1,13 @@
+package testMore;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class selenium {
+	@Test
+	public void moreTest(){
+		Assert.assertEquals(true, true);
+	}
+
+}
