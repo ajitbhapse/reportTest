714d5c5ef80cfcb7bf80a968a5fecb99021fdc37
ajit <ajitbhapse@gmail.com>
2017-03-22 00:11:20 +0530

    This is first project on Git

---
+++ src/test/java/test/test/AppTest.java
@@ -0,0 +1,20 @@
+package test.test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+	
+	@Test
+	public void testTrue(){
+		Assert.assertEquals(true, true);
+	}
+  @Test
+  public void testFalse(){
+	  Assert.assertEquals(true, false);
+  }
+}
